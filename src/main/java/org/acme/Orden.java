65dfc571bd0ade8fe1fb6a15bdec8afe4d116e5c
Joel <joelfigueiridomolares@gmail.com>
2024-05-21 14:09:10 +0200

    creacion ordenes

--- src/main/java/org/acme/Orden.java
+++ src/main/java/org/acme/Orden.java
@@ -1,10 +1,13 @@
 package org.acme;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+import java.util.stream.Collectors;
+import java.util.List;
 
 @Entity
     
@@ -34,5 +37,15 @@ public class Orden extends PanacheEntityBase {
     public Item getItem(){
         return item;
     }
+    
+
+    public static List<Orden> findByUserName(String name) {
+		List<Orden> ordenes = Orden.listAll();
+		List<Orden> ordenesByName = ordenes.stream()
+										.filter(o -> o.getUser().getNombre().equalsIgnoreCase(name))
+										.collect(Collectors.toList());
+		return ordenesByName.isEmpty()? List.of(): ordenesByName;
+	}
+
     
 }
---
+++ src/main/java/org/acme/ServiceOlli.java
@@ -0,0 +1,68 @@
+package org.acme;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
+public class ServiceOlli {
+    public ServiceOlli(){}
+
+    public Usuaria cargaUsuaria(String nombre) {
+        Optional<Usuaria> usuaria = Usuaria.findByIdOptional(nombre);
+        if (usuaria.isPresent()) {
+            return usuaria.get();
+        } else {
+            return new Usuaria();
+        }
+        /*return usuaria.isPresent()? usuaria.get(): new Usuaria();*/
+    }
+    public Item cargaItem(String nombre) {
+        Optional<Item> item = Item.findByIdOptional(nombre);
+        if (item.isPresent()) {
+            return item.get();
+        } else {
+            return new Item();
+        }
+        /*return item.isPresent()? item.get(): new Item();*/
+    }
+    public List<Orden> cargaOrden(String nombre) {
+        return Orden.findByUserName(nombre);
+    }
+    @Transactional
+    public Orden comanda(String usuaria_nombre, String item_nombre) {
+        Orden orden = null;
+        Optional<Usuaria> usuaria = Usuaria.findByIdOptional(usuaria_nombre);
+        Optional<Item> item = Item.findByIdOptional(item_nombre);
+        if (usuaria.isPresent() && item.isPresent() 
+            && usuaria.get().getDestreza() >= item.get().getQuality()) {
+            orden = new Orden(usuaria.get(), item.get());
+            orden.persist();
+        }
+        return orden;
+    }
+    @Transactional
+    public List<Orden> comandaMultiple(String usuaria, List<String> productos) {
+
+        Optional<Usuaria> user = Usuaria.findByIdOptional(usuaria);
+        if (user.isEmpty()) {
+            return Collections.emptyList();
+        }
+        
+        List<Orden> ordenes = new ArrayList<Orden>();
+
+        Orden orden = null;
+        for (String producto: productos) {
+            orden = this.comanda(user.get().getNombre(), producto);
+            if (orden != null) {
+                ordenes.add(orden);
+            }
+        }
+        return ordenes;      
+    }
+}
+
+}
