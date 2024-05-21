4dba4d2d5ef7eaee00283ceba304a1929011034e
Joel <joelfigueiridomolares@gmail.com>
2024-05-21 12:35:38 +0200

    Test copiados y proyecto creado

---
+++ src/test/java/org/acme/ResourcesTest.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ResourcesTest extends ServiceTest {
+    // Execute the same tests but in packaged mode.
+}
